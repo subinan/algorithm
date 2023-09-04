635aa469d73d36893bd6ee14f13e58dced951853
안수빈 <060657@naver.com>
2023-09-04 13:55:57 +0900

    add: 230904 백준 1695 팰린드롬 만들기

--- src/boj/BJ_1695_팰린드롬만들기.java
+++ src/boj/BJ_1695_팰린드롬만들기.java
@@ -1,2 +1,35 @@
-package boj;public class BJ_1695_팰린드롬만들기 {
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1695_팰린드롬만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[N][N];
+        for (int i = N - 1; i >= 0; i--) {
+            dp[i][i] = 0;
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] == arr[j]) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        System.out.println(dp[0][N - 1]);
+    }
 }
